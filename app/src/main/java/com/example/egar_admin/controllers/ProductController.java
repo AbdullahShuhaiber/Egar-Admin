1e05ae008613c21094e8a1179b68f7503fccfcb5
jaber <jaberqasem75@gmail.com>
2023-05-05 20:07:12 +0300

    Models & Controllers Created By Qasem

---
+++ app/src/main/java/com/example/egar_admin/controllers/ProductController.java
@@ -0,0 +1,15 @@
+package com.example.egar_admin.controllers;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.core.FirestoreClient;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+
+public class ProductController {
+
+
+
+}
+

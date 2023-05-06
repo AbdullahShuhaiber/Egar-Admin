65588dfe6b8bb1f28d289597b5219cbcdf9a612a
jaber <jaberqasem75@gmail.com>
2023-05-07 01:32:23 +0300

    Models & Controllers Created By Qasem

--- app/src/main/java/com/example/egar_admin/Model/Order.java
+++ app/src/main/java/com/example/egar_admin/Model/Order.java
@@ -1,22 +1,26 @@
 package com.example.egar_admin.Model;
 
+import java.util.Date;
+
 public class Order {
     private String orderId;
     private String customerId;
     private String serviceId;
     private int quantity;
     private double totalAmount;
+    private Date orderDate ;
 
     public Order() {
         // Required empty constructor for Firebase
     }
 
-    public Order(String orderId, String customerId, String serviceId, int quantity, double totalAmount) {
+    public Order(String orderId, String customerId, String serviceId, int quantity, double totalAmount,Date orderDate) {
         this.orderId = orderId;
         this.customerId = customerId;
         this.serviceId = serviceId;
         this.quantity = quantity;
         this.totalAmount = totalAmount;
+        this.orderDate=orderDate;
     }
 
     // Getters and Setters
@@ -58,6 +62,14 @@ public class Order {
 
     public void setTotalAmount(double totalAmount) {
         this.totalAmount = totalAmount;
+    }
+
+    public Date getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(Date orderDate) {
+        this.orderDate = orderDate;
     }
 }
 

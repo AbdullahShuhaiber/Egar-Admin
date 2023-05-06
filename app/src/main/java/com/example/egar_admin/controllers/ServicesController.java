dfb1679dffc0b3e711f9559eb6a3b313cfdb9b42
jaber <jaberqasem75@gmail.com>
2023-05-06 23:45:08 +0300

    Models & Controllers Created By Qasem

--- app/src/main/java/com/example/egar_admin/controllers/ServicesController.java
+++ app/src/main/java/com/example/egar_admin/controllers/ServicesController.java
@@ -1,4 +1,33 @@
 package com.example.egar_admin.controllers;
 
+import android.app.Service;
+import java.util.List;
+import java.util.Optional;
+
 public class ServicesController {
+
+
+
+
+    public Optional<Service> getServiceById(String id) {
+        // TODO: implement this method
+        return null;
+    }
+
+    public List<Service> getAllServices() {
+        // TODO: implement this method
+        return null;
+    }
+
+    public void saveService(Service service) {
+        // TODO: implement this method
+    }
+
+    public void updateService(String id, Service updatedService) {
+        // TODO: implement this method
+    }
+
+    public void deleteService(String id) {
+        // TODO: implement this method
+    }
 }

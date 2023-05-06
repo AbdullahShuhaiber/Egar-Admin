242b89a0d430dc51626a3aabf7da2b8452b13a9d
jaber <jaberqasem75@gmail.com>
2023-05-06 23:45:04 +0300

    Models & Controllers Created By Qasem

--- app/src/main/java/com/example/egar_admin/Model/Service.java
+++ app/src/main/java/com/example/egar_admin/Model/Service.java
@@ -1,94 +1,60 @@
 package com.example.egar_admin.Model;
 
 public class Service {
-    private String serviceId;
-    private String serviceName;
-    private String serviceDescription;
-    private double servicePrice;
-    private int serviceRating;
-    private String imageUrl;
+    private String name;
+    private String description;
+    private double price;
+    private String image;
+    private boolean isFavorite;
+    private boolean isInCart;
 
-    public Service() {
-        // Required empty constructor for Firebase
+    public Service(String name, String description, double price, String image) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.image = image;
+        this.isFavorite = false;
+        this.isInCart = false;
     }
 
-    public Service(String serviceId, String serviceName, String serviceDescription, double servicePrice, int serviceRating, String imageUrl) {
-        this.serviceId = serviceId;
-        this.serviceName = serviceName;
-        this.serviceDescription = serviceDescription;
-        this.servicePrice = servicePrice;
-        this.serviceRating = serviceRating;
-        this.imageUrl = imageUrl;
+    public String getName() {
+        return name;
     }
 
-    // Getters and Setters
-    public String getServiceId() {
-        return serviceId;
+    public String getDescription() {
+        return description;
     }
 
-    public void setServiceId(String serviceId) {
-        this.serviceId = serviceId;
+    public double getPrice() {
+        return price;
     }
 
-    public String getServiceName() {
-        return serviceName;
+    public String getImage() {
+        return image;
     }
 
-    public void setServiceName(String serviceName) {
-        this.serviceName = serviceName;
+    public boolean isFavorite() {
+        return isFavorite;
     }
 
-    public String getServiceDescription() {
-        return serviceDescription;
+    public boolean isInCart() {
+        return isInCart;
     }
 
-    public void setServiceDescription(String serviceDescription) {
-        this.serviceDescription = serviceDescription;
+    public void addToFavorites() {
+        this.isFavorite = true;
     }
 
-    public double getServicePrice() {
-        return servicePrice;
+    public void removeFromFavorites() {
+        this.isFavorite = false;
     }
 
-    public void setServicePrice(double servicePrice) {
-        this.servicePrice = servicePrice;
+    public void addToCart() {
+        this.isInCart = true;
     }
 
-    public int getServiceRating() {
-        return serviceRating;
+    public void removeFromCart() {
+        this.isInCart = false;
     }
-
-    public void setServiceRating(int serviceRating) {
-        this.serviceRating = serviceRating;
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
-    }
-
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
-
-    // Store image in Firebase
-//    public void uploadImageToFirebaseStorage(Bitmap bitmap, String imageName, final OnSuccessListener<Uri> onSuccessListener, final OnFailureListener onFailureListener) {
-//        // Get Firebase storage reference
-//        FirebaseStorage storage = FirebaseStorage.getInstance();
-//        StorageReference storageRef = storage.getReference();
-//
-//        // Create reference to image file in Firebase storage
-//        StorageReference imageRef = storageRef.child("services/" + imageName + ".jpg");
-//
-//        // Convert image bitmap to byte array
-//        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-//        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
-//        byte[] imageData = baos.toByteArray();
-//
-//        // Upload image to Firebase storage
-//        UploadTask uploadTask = imageRef.putBytes(imageData);
-//
-//        // Add listeners for success and failure
-//        uploadTask.addOnSuccessListener(onSuccessListener).addOnFailureListener(onFailureListener);
-//    }
 }
 

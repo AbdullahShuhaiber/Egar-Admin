34126012cf4462882e5dff7d47cfdd764b29fc08
jaber <jaberqasem75@gmail.com>
2023-05-06 23:44:43 +0300

    Models & Controllers Created By Qasem

--- app/src/main/java/com/example/egar_admin/activity/LoginActivity.java
+++ app/src/main/java/com/example/egar_admin/activity/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.example.egar_admin.activity;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.ConnectivityManager;
@@ -9,6 +11,7 @@ import android.view.View;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import com.example.egar_admin.BroadcastReceivers.NetworkChangeListiners;
 import com.example.egar_admin.FirebaseManger.FirebaseAuthController;
@@ -16,6 +19,7 @@ import com.example.egar_admin.R;
 import com.example.egar_admin.databinding.ActivityLoginBinding;
 import com.example.egar_admin.interfaces.ProcessCallback;
 import com.example.egar_admin.ui.MainActivity;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 
 
@@ -31,111 +35,150 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     }
 
-        private void screenOperations (){
-            setOnClick();
-            setDataInInputFieldFromRegister();
-
-        }
-
-        @Override
-        protected void onStart() {
-            super.onStart();
-            IntentFilter intentFilter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
-            registerReceiver(networkChangeListiners,intentFilter);
-        }
-
-        @Override
-        protected void onResume() {
-            super.onResume();
-        }
-
-        @Override
-        protected void onPause() {
-            super.onPause();
-        }
-
-        @Override
-        protected void onStop() {
-            super.onStop();
-            unregisterReceiver(networkChangeListiners);
-        }
-
-        @Override
-        protected void onDestroy() {
-            super.onDestroy();
-        }
-
-        @Override
-        protected void onRestart() {
-            super.onRestart();
-        }
-
-        private  boolean setDataInInputFieldFromRegister (){
-            Intent intent = getIntent();
-            String phone = intent.getStringExtra("email");
-            String password =intent.getStringExtra("password");
-            if (password == null && phone == null){
-                return false;
-            }else {
-                binding.etPhone.setText(intent.getStringExtra("email"));
-                binding.etPassword.setText(intent.getStringExtra("password"));
-                return true;
+    @Override
+    public void onBackPressed() {
+        // Create an exit dialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Exit");
+        builder.setMessage("Are you sure you want to exit?");
+        builder.setIcon(R.drawable.baseline_exit_to_app_24);
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Close the application
+                finish();
             }
-        }
-        private boolean dataCheck (){
-            String phone = binding.etPhone.getText().toString();
-            String password = binding.etPassword.getText().toString();
-            if (phone.isEmpty()) {
-                binding.etPhone.setError("PhoneNumber field is Required");
-                return false;
-            } else if (password.isEmpty()) {
-                binding.etPassword.setError("Password field is Required");
-                return false;
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Dismiss the dialog and continue with the application
+                dialog.dismiss();
             }
+        });
+        // Create the dialog and show it
+        AlertDialog dialog = builder.create();
+        dialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
+            @Override
+            public void onDismiss(DialogInterface dialog) {
+                // Do nothing
+            }
+        });
+        dialog.show();
+    }
+
+
+    private void screenOperations (){
+        setOnClick();
+        setDataInInputFieldFromRegister();
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        IntentFilter intentFilter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+        registerReceiver(networkChangeListiners,intentFilter);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(networkChangeListiners);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    private  boolean setDataInInputFieldFromRegister (){
+        Intent intent = getIntent();
+        String email = intent.getStringExtra("email");
+        String password =intent.getStringExtra("password");
+        if (password == null && email == null){
+            return false;
+        }else {
+            binding.etEmail.setText(intent.getStringExtra("email"));
+            binding.etPassword.setText(intent.getStringExtra("password"));
             return true;
         }
-        private  void setOnClick(){
-            binding.btnLogin.setOnClickListener(this::onClick);
-            binding.tvCreateAccount.setOnClickListener(this::onClick);
-           // binding.btnBack.setOnClickListener(this::onClick);
-            binding.tvForgotPassword.setOnClickListener(this::onClick);
+    }
+    private boolean dataCheck (){
+        String email = binding.etEmail.getText().toString();
+        String password = binding.etPassword.getText().toString();
+        if (email.isEmpty()) {
+            binding.etEmail.setError("Email field is Required");
+            return false;
+        } else if (password.isEmpty()) {
+            binding.etPassword.setError("Password field is Required");
+            return false;
         }
-        @SuppressLint("NonConstantResourceId")
-        @Override
-        public void onClick(View view) {
-            switch (view.getId()){
-                case R.id.btn_login:
-                    if (dataCheck()){
+        return true;
+    }
+
+    private  void setOnClick(){
+        binding.btnLogin.setOnClickListener(this::onClick);
+        binding.tvCreateAccount.setOnClickListener(this::onClick);
+        binding.tvForgotPassword.setOnClickListener(this::onClick);
+    }
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_login:
+                if (dataCheck()){
+                    login();
+                }else {
+                    Snackbar.make(binding.getRoot(), "Please enter Data , The Input Filed is Required", Snackbar.LENGTH_LONG).setTextColor(ContextCompat.getColor(this, R.color.bronze)).show();
+                }
+                break;
+            case R.id.tv_createAccount:
+                Intent intent1 = new Intent(getApplicationContext(),RegisterActivity.class);
+                startActivity(intent1);
+                break;
+            case R.id.tv_forgotPassword:
+                Intent intent3 = new Intent(getApplicationContext(),ResetPassword.class);
+                startActivity(intent3);
+                break;
+
+
+        }
+    }
+    private void login() {
+        FirebaseAuthController.getInstance().signIn(binding.etEmail.getText().toString(),
+                binding.etPassword.getText().toString(),
+                new ProcessCallback() {
+                    @Override
+                    public void onSuccess(String message) {
+                        Snackbar.make(binding.getRoot(),message,Snackbar.LENGTH_LONG).show();
                         Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                         startActivity(intent);
-                        login();
-                    }else {
-                        Toast.makeText(this, "The Input Fields Required", Toast.LENGTH_SHORT).show();
+                        finish();
                     }
-                    break;
-                case R.id.tv_createAccount:
-                    Intent intent1 = new Intent(getApplicationContext(),RegisterActivity.class);
-                    startActivity(intent1);
-                    break;
-       /*         case R.id.tv_forgotPassword:
-                    Intent intent3 = new Intent(getApplicationContext(),ResetPassword.class);
-                    startActivity(intent3);
-                    break;*/
 
-            }
-        }
-        private void login(){
-            FirebaseAuthController.getInstance().signIn(binding.etPhone.getText().toString().trim(), binding.etPassword.getText().toString().trim(), new ProcessCallback() {
-                @Override
-                public void onSuccess(String message) {
-                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                    startActivity(intent);
-                }
+                    @Override
+                    public void onFailure(String message) {
+                        Snackbar.make(binding.getRoot(),message,Snackbar.LENGTH_LONG).show();
 
-                @Override
-                public void onFailure(String message) {
+                    }
+                });
+    }
 
-                }
-            });
-        }
+
 }

241068d39b17a9a74297187816cd9dd652b73707
jaber <jaberqasem75@gmail.com>
2023-05-21 22:54:34 +0300

    Preparing designs and functions for delivery

--- app/src/main/java/com/example/egar_admin/activity/DeliveryActivity.java
+++ app/src/main/java/com/example/egar_admin/activity/DeliveryActivity.java
@@ -1,10 +1,16 @@
 package com.example.egar_admin.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import com.example.egar_admin.R;
+import com.example.egar_admin.fragment.DeliveryFragment.DeliveryOrdersFragment;
+import com.example.egar_admin.fragment.DeliveryFragment.DeliveryProfileFragment;
+import com.example.egar_admin.fragment.DeliveryFragment.DeliveryWalletFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class DeliveryActivity extends AppCompatActivity {
 
@@ -13,4 +19,27 @@ public class DeliveryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_delivery);
     }
+    @SuppressLint("NonConstantResourceId")
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = item -> {
+        Fragment selectedFragment = null;
+
+        switch (item.getItemId()) {
+            case R.id.menu_orders:
+                selectedFragment = new DeliveryOrdersFragment();
+                break;
+            case R.id.menu_profile:
+                selectedFragment = new DeliveryProfileFragment();
+                break;
+            case R.id.menu_wallet:
+                selectedFragment = new DeliveryWalletFragment();
+                break;
+        }
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.content_container, selectedFragment)
+                .commit();
+
+        return true;
+    };
 }

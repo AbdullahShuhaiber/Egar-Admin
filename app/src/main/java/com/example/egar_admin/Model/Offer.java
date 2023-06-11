a276c465a7428b626112dc0c6ec608af676a3c52
jaber <jaberqasem75@gmail.com>
2023-06-12 00:10:42 +0300

    build

--- app/src/main/java/com/example/egar_admin/Model/Offer.java
+++ app/src/main/java/com/example/egar_admin/Model/Offer.java
@@ -5,27 +5,23 @@ import java.util.Date;
 public class Offer {
     private String id;
     private String productId;
-    private String providerId;
     private double price;
     private int quantity;
     private Date startDate;
     private Date endDate;
 
     public Offer() {
-        // البناء الافتراضي المطلوب للتعامل مع Firebase
     }
 
-    public Offer(String id, String productId, String providerId, double price, int quantity, Date startDate, Date endDate) {
+    public Offer(String id, String productId, double price, int quantity, Date startDate, Date endDate) {
         this.id = id;
         this.productId = productId;
-        this.providerId = providerId;
         this.price = price;
         this.quantity = quantity;
         this.startDate = startDate;
         this.endDate = endDate;
     }
 
-    // Getter و Setter للحصول على القيم وتعيينها
 
     public String getId() {
         return id;
@@ -43,13 +39,7 @@ public class Offer {
         this.productId = productId;
     }
 
-    public String getProviderId() {
-        return providerId;
-    }
 
-    public void setProviderId(String providerId) {
-        this.providerId = providerId;
-    }
 
     public double getPrice() {
         return price;

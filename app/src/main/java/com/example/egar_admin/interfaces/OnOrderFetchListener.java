2da0b027829282cac333c1890a951acacf0f040d
jaber <jaberqasem75@gmail.com>
2023-06-22 15:42:14 +0300

    .

---
+++ app/src/main/java/com/example/egar_admin/interfaces/OnOrderFetchListener.java
@@ -0,0 +1,17 @@
+package com.example.egar_admin.interfaces;
+
+import com.example.egar_admin.Model.Order;
+
+import java.util.List;
+
+public interface OnOrderFetchListener {
+    void onAddOrderSuccess(String orderId);
+    void onAddOrderFailure(String message);
+    void onUpdateOrderSuccess();
+    void onUpdateOrderFailure(String message);
+    void onDeleteOrderSuccess();
+    void onDeleteOrderFailure(String message);
+    void onGetOrdersByServiceProviderIdSuccess(List<Order> orders);
+    void onGetOrdersByServiceProviderIdFailure(String message);
+}
+

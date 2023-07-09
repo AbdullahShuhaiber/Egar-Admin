bb4b992570801479d801f57bdb9c8abaa671dc26
AbdullahShuhaiber <abdullahahed33@gmail.com>
2023-07-09 11:51:12 +0300

    app

--- app/src/main/java/com/example/egar_admin/ui/MainActivity.java
+++ app/src/main/java/com/example/egar_admin/ui/MainActivity.java
@@ -58,7 +58,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_orders , R.id.nav_add_product)
+                R.id.nav_home, R.id.nav_orders , R.id.nav_add_product,R.id.nav_settings,R.id.nav_Conditions,R.id.nav_WhoAreWe)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);

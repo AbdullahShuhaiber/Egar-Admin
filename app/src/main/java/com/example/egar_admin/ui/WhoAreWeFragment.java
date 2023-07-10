8babd388ac77d73e7bf3fd661e3bc5492a669dd0
jaber <jaberqasem75@gmail.com>
2023-07-10 15:06:01 +0300

    .

--- app/src/main/java/com/example/egar_admin/ui/WhoAreWeFragment.java
+++ app/src/main/java/com/example/egar_admin/ui/WhoAreWeFragment.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.egar_admin.R;
+import com.example.egar_admin.databinding.FragmentWhoAreWeBinding;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -22,6 +23,7 @@ public class WhoAreWeFragment extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
+    FragmentWhoAreWeBinding binding;
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
@@ -61,6 +63,22 @@ public class WhoAreWeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_who_are_we, container, false);
+        binding  = FragmentWhoAreWeBinding.inflate(inflater);
+        binding.tvWhoAreWeInEgar.setText(text());
+        return binding.getRoot();
+    }
+    private String text(){
+        String text = "في تطبيق Egar، نحن فخورون بأن نكون منصة رائدة في مجال تأجير المنازل ومساحات العمل والسيارات والمعدات. نحن نسعى جاهدين لتلبية احتياجاتك وتوفير تجربة تأجير فريدة ومريحة لعملائنا.\n" +
+                "\n" +
+                "تعتبر Egar خيارًا موثوقًا وموفرًا للوقت للعديد من الأسباب. نحن نقدم مجموعة واسعة من الخيارات لتلبية احتياجاتك الفردية والتجارية. سواء كنت تبحث عن منزل أحلامك أو مكان للعمل المناسب لنشاطك التجاري، فإن لدينا ما يناسبك.\n" +
+                "\n" +
+                "نحن نؤمن بأهمية التنوع والتكيف مع تطلعات عملائنا المتنوعة. لذلك، نقدم مجموعة متنوعة من العقارات التي تتضمن المنازل الفاخرة، والشقق المريحة، ومساحات العمل الحديثة، والسيارات المتنوعة، والمعدات المتخصصة. نحن نعمل مع شبكة واسعة من الملاك والشركاء لضمان توافر الخيارات المناسبة لكل عميل.\n" +
+                "\n" +
+                "منصتنا سهلة الاستخدام وتمنحك قوة الاختيار والتحكم الكامل في عملية التأجير. يمكنك الاطلاع على تفاصيل كل عقار بما في ذلك الصور والوصف والتواريخ المتاحة. يمكنك حجز العقار المفضل لديك بسهولة وسرعة عبر تطبيقنا المبتكر.\n" +
+                "\n" +
+                "نحن نضع عملائنا في المقام الأول ونولي اهتمامًا كبيرًا لتجربتهم. فريق دعم العملاء لدينا متواجد على مدار الساعة للإجابة على استفساراتك وتقديم المساعدة في أي مشكلة قد تواجهها. نحن نسعى جاهدين لتوفير خدمة عملاء استثنائية وضمان رضاك التام.\n" +
+                "\n" +
+                "انضم إلى Egar اليوم واستمتع بتجربة تأجير مميزة. دعنا نساعدك في تحقيق احتياجاتك السكنية والتجارية بسهولة ويسر. ";
+        return text;
     }
 }
